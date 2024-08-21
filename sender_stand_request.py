2013c4e81578b32c820466942a446ed52e3b9e41
Olga5642-art <ylukamarchenko@yandex.ru>
2024-08-21 14:13:07 +0100

    Исправление SQL-запроса во втором задании+исправление автотеста

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,11 +8,6 @@ def post_new_zakaz(body):
     # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ZAKAZ_PATH,
                          json=body)
-# Вызов функции post_new_zakaz
-#response_zakaz = post_new_zakaz(data.zakaz_body)
-# Получение трекере созданного заказа
-#response_track =response_zakaz.json()['track']
-#print(response_track)
 
 # Определение функции для отправки GET-запроса на получение заказа по треку
 def get_zakaz_by_track (track):
